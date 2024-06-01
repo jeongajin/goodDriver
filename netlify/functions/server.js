bd7a0dd51a15108176254ace023404539290193b
jeongajin <jdo999@naver.com>
2024-06-01 20:07:39 +0900

    서버 수정

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,20 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+// 서버리스 함수의 특성상, 각 요청을 함수로 처리해야 합니다.
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.get("/help", function (req, res) {
-    res.sendFile(__dirname + '/public/help.html');
+    res.sendFile(path.join(__dirname, '../public', 'help.html'));
 });
 
 app.get("/records", function (req, res) {
-    res.sendFile(__dirname + '/public/records.html');
+    res.sendFile(path.join(__dirname, '../public', 'records.html'));
 });
 
 app.get("/settings", function (req, res) {
-    res.sendFile(__dirname + '/public/settings.html');
+    res.sendFile(path.join(__dirname, '../public', 'settings.html'));
 });
 
+// Netlify Functions의 엔드포인트로 export
 module.exports.handler = serverless(app);
