0347abc9e6680f632e4afbf59a1068bba7acb712
jeongajin <jdo999@naver.com>
2024-06-01 20:15:19 +0900

    서버 수정

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ let user_id = 'user1';
 let friend_id = 'user2';
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 app.use(express.static("public"));
 
 // 서버리스 함수의 특성상, 각 요청을 함수로 처리해야 합니다.
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+    res.render('index.ejs');
 });
 
 app.get("/help", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'help.html'));
+    res.render('help.html');
 });
 
 app.get("/records", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'records.html'));
+    res.render('records.html');
 });
 
-app.get("/settings", function (req, res) {
-    res.sendFile(path.join(__dirname, '../public', 'settings.html'));
+app.get("/setttings", function (req, res) {
+    res.render('settings.html');
 });
 
 // Netlify Functions의 엔드포인트로 export
