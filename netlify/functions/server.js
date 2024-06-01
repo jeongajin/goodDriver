fddbeae8093f47f6ac778488f6f823e265fb985e
jeongajin <jdo999@naver.com>
2024-06-01 20:05:12 +0900

    서버 수정

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,34 +1,29 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const multer = require('multer');
 const bodyParser = require('body-parser');
 
 let user_id = 'user1';
 let friend_id = 'user2';
 
-app.listen(8080, function () {
-    console.log("포트 8080으로 서버 대기중 ... ");
-});
-
-app.use(express.static('public'));
-
-//body-parser 라이브러리 추가
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.render('home.ejs');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get("/help", function (req, res) {
-    res.render('help.ejs');
+    res.sendFile(__dirname + '/public/help.html');
 });
 
 app.get("/records", function (req, res) {
-    res.render('records.ejs');
+    res.sendFile(__dirname + '/public/records.html');
 });
 
 app.get("/settings", function (req, res) {
-    res.render('settings.ejs');
+    res.sendFile(__dirname + '/public/settings.html');
 });
+
+module.exports.handler = serverless(app);
